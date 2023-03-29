303d8ae5ee0e8824b18550b201739df1d157dbc1
Francisco <franjcasis@gmail.com>
2023-03-29 09:57:26 -0300

    Ejercicio 6

--- src/Unidad0/ReemplazoLetra.java
+++ src/Unidad0/ReemplazoLetra.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class ReemplazoLetra {
     public static void main(String[] args) {
         char letraReemplazo;
-        Scanner entrada=new Scanner(System.in);
-        String nuevaOracion="",oracion="Ayer me compré muñecos de la marca ‘Colchitas’ por internet";
+        Scanner entrada = new Scanner(System.in);
+        String nuevaOracion = "", oracion = "Ayer me compré muñecos de la marca ‘Colchitas’ por internet";
         System.out.println("Introduzca la letra de reemplazo:");
-        letraReemplazo=entrada.next().charAt(0);
-        for(int i=0;i<oracion.length();i++) {
-            if(oracion.charAt(i)=='e') {
-                nuevaOracion+=letraReemplazo;
-            } else if(oracion.charAt(i)=='E') {
-                nuevaOracion+=Character.toUpperCase(letraReemplazo);
+        letraReemplazo = entrada.next().charAt(0);
+        for (int i = 0; i < oracion.length(); i++) {
+            if (oracion.charAt(i) == 'e') {
+                nuevaOracion += letraReemplazo;
+            } else if (oracion.charAt(i) == 'E') {
+                nuevaOracion += Character.toUpperCase(letraReemplazo);
             } else {
-                nuevaOracion+=oracion.charAt(i);
+                nuevaOracion += oracion.charAt(i);
             }
         }
-        System.out.println("Oración original: "+"\""+oracion+"\"");
-        System.out.println("Oración resultante: "+"\""+nuevaOracion+"\"");
+        System.out.println("Oración original: " + "\"" + oracion + "\"");
+        System.out.println("Oración resultante: " + "\"" + nuevaOracion + "\"");
     }
 }
