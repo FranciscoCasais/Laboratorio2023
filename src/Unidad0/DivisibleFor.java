f4fa50cb80119b52e18959bbf0ce413bf1deaac9
Francisco <franjcasis@gmail.com>
2023-03-29 09:55:44 -0300

    Ejercicio 3

--- src/Unidad0/DivisibleFor.java
+++ src/Unidad0/DivisibleFor.java
@@ -1,8 +1,8 @@
 package Unidad0 ;
 public class DivisibleFor {
     public static void main(String[] args) {
-        for(int i=1;i<=100;i++) {
-            if((i%2==0)&&(i%3==0)) {
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 2 == 0) && (i % 3 == 0)) {
                 System.out.println(i);
             }
         }
