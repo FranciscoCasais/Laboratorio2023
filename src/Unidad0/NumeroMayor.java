d87b240f5ddb3e4f0a6de4a90eb4bdf858195c64
Francisco <franjcasis@gmail.com>
2023-03-29 09:55:10 -0300

    Ejercicio 2

--- src/Unidad0/NumeroMayor.java
+++ src/Unidad0/NumeroMayor.java
@@ -2,15 +2,15 @@ package Unidad0;
 import java.util.Scanner;
 public class NumeroMayor {
     public static void main(String[] args) {
-        int numero1,numero2;
-        Scanner entrada=new Scanner(System.in);
+        int numero1, numero2;
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Introduzca valor para el primer número: ");
-        numero1=entrada.nextInt();
+        numero1 = entrada.nextInt();
         System.out.println("Introduzca valor para el segundo número: ");
-        numero2=entrada.nextInt();
-        if(numero1>numero2) {
+        numero2 = entrada.nextInt();
+        if (numero1 > numero2) {
             System.out.println("El primer número es mayor");
-        } else if(numero2>numero1) {
+        } else if (numero2 > numero1) {
             System.out.println("El segundo número es mayor");
         } else {
             System.out.println("Ambos números son equivalentes");
