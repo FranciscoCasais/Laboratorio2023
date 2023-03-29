fa5cbcb1ae0b1cb29712e3f105b2f8060d636465
Francisco <franjcasis@gmail.com>
2023-03-29 09:58:44 -0300

    Ejercicio 8

--- src/Unidad0/PalabrasIguales.java
+++ src/Unidad0/PalabrasIguales.java
@@ -2,12 +2,12 @@ package Unidad0;
 import java.util.Scanner;
 public class PalabrasIguales {
     public static void main(String[] args) {
-        Scanner entrada=new Scanner(System.in);
-        String palabra1,palabra2;
+        Scanner entrada = new Scanner(System.in);
+        String palabra1, palabra2;
         System.out.println("Ingrese dos palabras:");
-        palabra1=entrada.nextLine();
-        palabra2=entrada.nextLine();
-        if(palabra1.equals(palabra2)) {
+        palabra1 = entrada.nextLine();
+        palabra2 = entrada.nextLine();
+        if (palabra1.equals(palabra2)) {
             System.out.println("Las palabras son iguales");
         } else {
             System.out.println("Las palabras no son iguales");
