a79d7056807c345db548e9dfb6390888dac82cde
Francisco <franjcasis@gmail.com>
2023-03-31 10:24:04 -0300

    Ejercicio 4

--- src/Unidad1/Persona.java
+++ src/Unidad1/Persona.java
@@ -2,18 +2,38 @@ package Unidad1;
 public class Persona {
     private int edad;
     private String direccion, nombreCompleto;
+
     public Persona() {
-        edad=0;
-        direccion="";
-        nombreCompleto ="";
+        edad = 0;
+        direccion = "";
+        nombreCompleto = "";
     }
-    public int getEdad() { return edad; }
-    public String getDireccion() { return direccion; }
-    public String getNombreCompleto() { return nombreCompleto; }
-    public void setEdad(int edad) { this.edad=edad; }
-    public void setDireccion(String direccion) { this.direccion=direccion; }
-    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
     public void getDatos() {
-        System.out.println("Datos de la persona:\nNombre completo: "+ nombreCompleto +"\nEdad: "+edad+"\nDirección: "+direccion);
+        System.out.println("Datos de la persona:\nNombre completo: " + nombreCompleto + "\nEdad: " + edad + "\nDirección: " + direccion);
     }
 }
