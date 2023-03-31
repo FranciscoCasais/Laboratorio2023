b27c02699cad4d2da4dd49884cc0cb5b9f4d5f26
Francisco <franjcasis@gmail.com>
2023-03-31 10:25:33 -0300

    Ejercicio 7

--- src/Unidad1/Cafetera.java
+++ src/Unidad1/Cafetera.java
@@ -1,41 +1,64 @@
 package Unidad1;
 public class Cafetera {
-    private double cantidadActual,cantidadMaxima;
+    private double cantidadActual, cantidadMaxima;
+
     public Cafetera() {
-        cantidadActual=0;
-        cantidadMaxima=1000;
+        cantidadActual = 0;
+        cantidadMaxima = 1000;
     }
-    public Cafetera(double cantidadMaxima) { this.cantidadMaxima=cantidadMaxima; }
-    public Cafetera(double cantidadActual,double cantidadMaxima) {
-        if(cantidadActual>cantidadMaxima) {
-            this.cantidadActual=cantidadMaxima;
+
+    public Cafetera(double cantidadMaxima) {
+        this.cantidadMaxima = cantidadMaxima;
+    }
+
+    public Cafetera(double cantidadActual, double cantidadMaxima) {
+        if (cantidadActual > cantidadMaxima) {
+            this.cantidadActual = cantidadMaxima;
         } else {
-            this.cantidadActual=cantidadActual;
+            this.cantidadActual = cantidadActual;
         }
-        this.cantidadMaxima=cantidadMaxima;
+        this.cantidadMaxima = cantidadMaxima;
     }
-    public double getCantidadActual() { return cantidadActual; }
-    public double getCantidadMaxima() { return cantidadMaxima; }
+
+    public double getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public double getCantidadMaxima() {
+        return cantidadMaxima;
+    }
+
     public void setCantidadActual(double cantidadActual) {
-        if(cantidadActual>cantidadMaxima) {
-            this.cantidadActual=cantidadMaxima;
+        if (cantidadActual > cantidadMaxima) {
+            this.cantidadActual = cantidadMaxima;
         } else {
-            this.cantidadActual=cantidadActual;
+            this.cantidadActual = cantidadActual;
         }
     }
-    public void setCantidadMaxima(double cantidadMaxima) { this.cantidadMaxima=cantidadMaxima; }
-    public void llenarCafetera() { cantidadActual=cantidadMaxima; }
+
+    public void setCantidadMaxima(double cantidadMaxima) {
+        this.cantidadMaxima = cantidadMaxima;
+    }
+
+    public void llenarCafetera() {
+        cantidadActual = cantidadMaxima;
+    }
+
     public void servirTaza(int capacidad) {
-        if(cantidadActual>=capacidad) {
-            cantidadActual-=capacidad;
+        if (cantidadActual >= capacidad) {
+            cantidadActual -= capacidad;
         } else {
-            cantidadActual=0;
+            cantidadActual = 0;
         }
     }
-    public void vaciarCafetera() { cantidadActual=0; }
+
+    public void vaciarCafetera() {
+        cantidadActual = 0;
+    }
+
     public void agregarCafe(int cantidad) {
-        if(cantidadActual+cantidad<=cantidadMaxima) {
-            cantidadActual+=cantidad;
+        if (cantidadActual + cantidad <= cantidadMaxima) {
+            cantidadActual += cantidad;
         } else {
             System.out.println("Error: cantidad máxima de la cafetera excedida.");
         }
