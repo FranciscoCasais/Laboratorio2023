f9815c6b3c726e804d4769041cea15091180f1b0
Francisco <franjcasis@gmail.com>
2023-03-31 10:19:26 -0300

    Ejercicio 1

--- src/Unidad1/Circulo.java
+++ src/Unidad1/Circulo.java
@@ -2,20 +2,28 @@ package Unidad1;
 import java.lang.Math;
 public class Circulo {
     private double radio;
-    public Circulo() { radio=2.0; }
-    public Circulo(double radio) {
-        this.radio=radio;
+
+    public Circulo() {
+        radio = 2.0;
     }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
     public double getRadio() {
         return radio;
     }
+
     public void setRadio(double radio) {
-        this.radio=radio;
+        this.radio = radio;
     }
+
     public double calcularArea() {
-        return Math.PI*Math.pow(radio,2);
+        return Math.PI * Math.pow(radio, 2);
     }
+
     public double calcularPerimetro() {
-        return 2*Math.PI*radio;
+        return 2 * Math.PI * radio;
     }
 }
