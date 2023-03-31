2cb9439a48844403c48ecfbd14e415495c60ad14
Francisco <franjcasis@gmail.com>
2023-03-31 10:25:57 -0300

    Ejercicio 8

--- src/Unidad1/Cancion.java
+++ src/Unidad1/Cancion.java
@@ -2,16 +2,30 @@ package Unidad1;
 public class Cancion {
     private Persona autor;
     private String titulo;
+
     public Cancion() {
-        autor=new Persona();
-        titulo="";
+        autor = new Persona();
+        titulo = "";
     }
-    public Cancion(Persona autor,String titulo) {
-        this.autor=autor;
-        this.titulo=titulo;
+
+    public Cancion(Persona autor, String titulo) {
+        this.autor = autor;
+        this.titulo = titulo;
     }
-    public Persona getAutor() { return autor; }
-    public String getTitulo() { return titulo; }
-    public void setAutor(Persona autor) { this.autor=autor; }
-    public void setTitulo(String titulo) { this.titulo=titulo; }
+
+    public Persona getAutor() {
+        return autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutor(Persona autor) {
+        this.autor = autor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
