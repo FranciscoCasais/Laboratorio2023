82a5b6599bbe4c2140ddf2ca458d07d36e7092a1
Francisco <franjcasis@gmail.com>
2023-04-17 11:38:59 -0300

    Ejercicio 4

---
+++ src/Unidad3/Ejercicio4/DetectorHumo.java
@@ -0,0 +1,5 @@
+package Unidad3.Ejercicio4;
+public class DetectorHumo extends Sensor {
+    public DetectorHumo() { super(); }
+    public void dispararAlarma() { System.out.println("Llamando a los bomberos..."); }
+}
---
+++ src/Unidad3/Ejercicio4/SensorTemperatura.java
@@ -0,0 +1,5 @@
+package Unidad3.Ejercicio4;
+public class SensorTemperatura extends Sensor {
+    public SensorTemperatura() { super(); }
+    public void dispararAlarma() { System.out.println("¡Cuidado! La temperatura sube"); }
+}
