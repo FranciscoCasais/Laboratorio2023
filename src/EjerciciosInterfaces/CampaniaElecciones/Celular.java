dd660d60e02d2a1505f40480edf44c2b028343e7
FranciscoCasais <101185430+FranciscoCasais@users.noreply.github.com>
2023-10-25 09:58:19 -0300

    Delete src/EjerciciosInterfaces/CampaniaElecciones/Celular.java

--- src/EjerciciosInterfaces/CampaniaElecciones/Celular.java
+++
@@ -1,7 +0,0 @@
-package EjerciciosInterfaces.CampaniaElecciones;
-public class Celular {
-    private CompaniaTelefonica companiaTelefonica;
-    private int numeroSerie;
-    private long numero;
-    private String fabricante,modelo;
-}
